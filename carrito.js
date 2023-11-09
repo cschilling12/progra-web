c6ced728e431e61913ac6f9e16a75613a7a38b4e
cschilling12 <cschiling@itba.edu.ar>
2023-11-09 20:42:42 -0300

    nuevos cambios

---
+++ carrito.js
@@ -0,0 +1,59 @@
+const CARRITO = []
+
+function agregarAlCarrito(producto){CARRITO.push(producto)
+    document.getElement
+
+}
+
+const baseDeDatos = [
+    {
+        id: 1,
+        nombre: "libroT1",
+        precio: 17.400,
+    },
+    {
+        id: 2, 
+        nombre: "libroT2",
+        precio: 11.500,
+    },
+    {
+        id: 3, 
+        nombre: "libroT3",
+        precio: 15.500,
+    },
+    {
+        id: 4, 
+        nombre: "libroT4",
+        precio: 4.200,
+    },
+    {
+        id: 5, 
+        nombre: "libroT5",
+        precio: 6.200,
+    },
+    {
+        id: 6, 
+        nombre: "libroT6",
+        precio: 8.900,
+    },
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 function myFunction() {alert("El libro se ha añadido al carrito de manera exitosa!");}
 
+
+
+
